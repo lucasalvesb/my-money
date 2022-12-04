14e44631037e7ab1ae0d64b5c2a575a8ceffbc04
Lucas Alves <lucasalves94@gmail.com>
2022-12-03 21:32:09 -0300

    Authentication and basic skeleton of application

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+import 'firebase/compat/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQygtLHyFGB34AWjeCq0DfB6_SAfsCDTM",
+  authDomain: "mymoney-31637.firebaseapp.com",
+  projectId: "mymoney-31637",
+  storageBucket: "mymoney-31637.appspot.com",
+  messagingSenderId: "643724301852",
+  appId: "1:643724301852:web:a5b1d834d2479a983fb1d9"
+};
+
+// init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+//init firestore
+
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AuthContextProvider } from './context/AuthContext'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
